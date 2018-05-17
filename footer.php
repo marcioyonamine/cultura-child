1fc0c533db46b9c2af016a53215a6a4da15026fb
Marcio Yonamine <marcioyonamine@gmail.com>
2018-05-17 09:08:10 -0300

    First Child

---
+++ footer.php
@@ -0,0 +1,11 @@
+    <footer class="site-footer">
+    <div id="conteudo" class="cols">
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("rodape") ) : ?>
+<?php endif ;?> 
+</div>   
+    </footer><!-- /site-footer -->
+  
+    </div><!-- /main -->
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<div class="content">
+	<?php if( is_home() ){?>
+    <?php get_template_part('partials/slider'); ?>
+	<?php } ?>
+    <?php if (have_posts()) : ?>
+
+        <div class="grid  js-masonry" data-masonry-options='{ "columnWidth": ".grid-sizer", "gutter": ".gutter-sizer", "itemSelector": ".hentry", "stamp": ".sticky" }'>
+            <div class="grid-sizer"></div>
+            <div class="gutter-sizer"></div>
+
+            <?php get_template_part('partials/loop') ?>
+
+        </div><!-- /grid -->
+
+        <?php cultural_paging_nav(); ?>
+
+    <?php else : ?>
+
+        <?php get_template_part('content', 'none'); ?>
+
+    <?php endif; ?>
+</div><!-- /content -->
+
+<?php get_footer(); ?>
