7f61ee824c2b2b6d913ac545cc509495646e4a26
Marcio Yonamine <marcioyonamine@gmail.com>
2018-05-23 15:52:33 -0300

    desabilita angulajs

--- functions.php
+++ functions.php
@@ -4,4 +4,15 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() { 
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' ); 
 } 
+
+
+add_action( 'wp_print_scripts', 'remover_js', 100 );
+function remover_js() {
+	if(is_page('registro')){
+		wp_deregister_script( 'angular-app' );
+	}
+}
+
+
+
 ?>
